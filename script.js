e6b756378a3a79cac50abf35a5daa428df7f9c59
JMontesGonzalez <147007482+JMontesGonzalez@users.noreply.github.com>
2024-04-03 22:16:26 +0000

    first commit

---
+++ script.js
@@ -0,0 +1,11 @@
+// let main=document.querySelector("nav");
+// let home=document.querySelector(".home");
+// let about=document.querySelector(".about");
+// let project=document.querySelector(".project");
+// let contact=document.querySelector(".contact");
+
+// home.onclick=function(){
+   
+// home.style.textShadow="1px 1px 10px #fff, 1px 1px 10px #ccc";
+
+// }
